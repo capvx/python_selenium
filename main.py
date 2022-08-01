f309fd84bfdb6f5ce75f79a5417dbc330c2d3cbd
Vladimir Cap <86056310+capvx@users.noreply.github.com>
2022-08-01 13:27:04 +0200

    Add files via upload
    bot

--- main.py
+++ main.py
@@ -2,5 +2,18 @@ from booking.booking import Booking
 
 with Booking() as bot:
     bot.land_first_page()
+    # bot.change_currency(currency='EUR')
+    bot.select_place_to_go('Novi Sad')
+    bot.select_date(check_in_date='2022-08-17',
+                    check_out_date='2022-08-25')
+    bot.select_adults(30)
+    bot.click_search()
+
+
+
+
+
+
+
 
 
